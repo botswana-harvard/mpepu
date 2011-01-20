e40fac756a5d4ac2a8853a51f458ce1f190764c5
erik van widenfelt <ew2789@gmail.com>
2011-01-20 04:54:35 +0000

    git-svn-id: http://192.168.1.50/svn/bhp056_project@10 7d2a49fb-c946-49a1-8d64-222705d2c8ab

--- svnupdate.sh
+++ svnupdate.sh
@@ -1,44 +1,44 @@
 cd ~/bhp056
-cd bhp_choices bhp_choices
+cd bhp_choices
 svn commit -m ""
 svn update
 svn status
-cd ../bhp_fields bhp_fields
-svn commit -m ""
-svn update
-svn status
-
-cd ../bhp_validators bhp_validators
+cd ../bhp_fields
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_admin_models bhp_admin_models
+cd ../bhp_validators
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_consent_models bhp_consent
+cd ../bhp_admin_models
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_basic_models bhp_basic_models
+cd ../bhp_consent
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_clinic_models bhp_clinic_models
+cd ../bhp_basic_models
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_study_variables bhp_variables
+cd ../bhp_clinic_models
 svn commit -m ""
 svn update
 svn status
 
-cd ../bhp_code_lists bhp_code_lists
+cd ../bhp_variables
+svn commit -m ""
+svn update
+svn status
+
+cd ../bhp_code_lists 
 svn commit -m ""
 svn update
 svn status
@@ -47,4 +47,3 @@ cd ../bhp_lab bhp_lab
 svn commit -m ""
 svn update
 svn status
-
