d175318c10550b81f6a22cd0e2b39c63c5feba75
erik van widenfelt <ew2789@gmail.com>
2011-06-06 11:42:48 +0000

    git-svn-id: http://192.168.1.50/svn/bhp056_project@31 7d2a49fb-c946-49a1-8d64-222705d2c8ab

--- gen_admin.py
+++ gen_admin.py
@@ -1,32 +1,89 @@
+#!/usr/bin/env python
+import re
+from django.core.management import setup_environ
+
+try:
+    import settings
+except ImportError:
+    import sys
+    sys.stderr.write("Couldn't find the settings.py module.")
+    sys.exit(1)
+
+setup_environ(settings)
 
 def gen_admin( **kwargs):
+    
+    """generate text for admin.py given a list of models and a base model"""
+    
     from string import Template
+    from django.contrib.contenttypes.models import ContentType
 
-    model=kwargs.get("model")
-    base=kwargs.get("base")
+    app_label = kwargs.get("app_label", 'mpepu_maternal')
+    foreign_key_field = kwargs.get("foreign_key", "maternal_visit")
+    base_model_admin = kwargs.get("base", 'MyMaternalVisitModelAdmin')
 
-    s = Template("# ${model}\n\
-class ${model}Admin($base}): \n\
+    #app_label = 'mpepu_maternal'
+    #foreign_key_field = "registered_subject"
+    #base_model_admin = 'MyRegisteredSubjectModelAdmin'
+
+
+    s = Template("\n# ${model}\n\
+class ${model}Admin(${base}): \n\
 \n\
     form = ${model}Form\n\
 \n\
-    def save_model(self, request, obj, form, change):\n\
-        if change:\n\
-            obj.user_modified = request.user\n\
-            obj.save()\n\
-        if not change:\n\
-            obj.user_created = request.user\n\
-            obj.save()\n\
+    fields = (\n        ${fields}\n    )\n\
+\n\
+    radio_fields = {\n        ${radio_fields}\n    }\n\
+\n\
+    filter_horizontal = (\n        ${filter_horizontal}\n    )\n\
 \n\
 admin.site.register(${model}, ${model}Admin)")
 
-    print s.substitute(model=model,base=base)
+    model_names = []
+    contenttypes = ContentType.objects.all()
+    [model_names.append(ctype.model) for ctype in ContentType.objects.filter(app_label=app_label)]
+
+    # get model class from content type
+    for model_name in model_names:
+        model = ContentType.objects.get(model=model_name).model_class()
+        object_name = model._meta.__dict__['object_name']
+        for field in model._meta.fields:
+            if field.name == foreign_key_field:
+                # list fields        
+                fields = [] 
+                choice_fields = []   
+                for field in model._meta.fields:
+                    if field.name not in ['created', 'modified', 'user_created', 'user_modified', 'hostname_created', 'hostname_modified', 'id',]:
+                        fields.append('"'+field.name+'"')
+                    # if field has choices, add to choice_fields            
+                    if not field.__dict__['_choices'] == []:
+                       choice_fields.append('"%s":admin.VERTICAL' % field.name)
+                    try:
+                        if field.__dict__['related']:
+                            related.append(field.__dict__['parent_model']._meta.module_name)
+                    except:
+                        pass        
+                # if field has manytomany add to filter_horizontal
+                m2m_fields = []            
+                [m2m_fields.append('"'+f.name+'"') for f in model._meta.many_to_many]
+
+                print s.substitute(
+                    model = object_name,
+                    base = base_model_admin, 
+                    fields = ',\n        '.join(fields),        
+                    radio_fields = ',\n        '.join(choice_fields),        
+                    filter_horizontal = ',\n        '.join(m2m_fields),
+                    )
 
 def gen_form( **kwargs):
+
+    """generate text for forms.py"""
+
     from string import Template
 
     model=kwargs.get("model")
-    base=kwargs.get("base")
+    base_model_admin=kwargs.get("base")
     
     s = Template("# ${model}\n\
 class ${model}Form (${base}): \n\
@@ -39,8 +96,13 @@ class ${model}Form (${base}): \n\
     class Meta:\n\
         model = ${model}")
 
-    print s.substitute(model=model, base=base)
+    print s.substitute(model=model, base=base_model_admin)
 
+
+gen_admin()
+
+
+"""
 if __name__ == "__main__":
 
     from optparse import OptionParser
@@ -51,59 +113,13 @@ if __name__ == "__main__":
                       help="base model name", metavar="BASE_MODEL_NAME")
     (options, args) = parser.parse_args()
 
-    #gen_admin( model=options.model_name, base=options.base_model_name, )
-    """
-    models = ['InfantArvProph',
-        'InfantArvProphMod',
-        'InfantBirth',
-        'InfantBirthData',        
-        'InfantBirthExam',
-        'InfantBirthArv',
-        'InfantBirthFeed',
-        'InfantDeath',
-        'InfantDeathAssessment',
-        'InfantEligibilityFeedingChoice',
-        'InfantEligibility',
-        'InfantHaart ',
-        'InfantHaartMod',
-        'InfantNvpAdherence',
-        'InfantOffDrug',
-        'InfantPrerandoLoss',
-        'InfantStudyDrugInit',
-        'InfantSurvival',
-        'InfantVisit',]
-    """
-    models = ['MaternalArvPost ',
-    'MaternalArvPostMod',
-    'MaternalArvPostAdh',
-    'MaternalArvPreg ',
-    'MaternalArvPregHistory',
-    'MaternalConsent',
-    'MaternalDeath',
-    'MaternalDeathAssessment',
-    'MaternalDeath',
-    'MaternalEligiblityPost',
-    'MaternalEligiblityAnte',
-    'MaternalEnroll',
-    'MaternalEnrollDem',
-    'MaternalEnrollOb',
-    'MaternalEnrollMed',
-    'MaternalEnrollDx',
-    'MaternalEnrollArv',
-    'MaternalEnrollClin',
-    'MaternalLabDel',
-    'MaternalLabDelMed        ',
-    'MaternalLabDelDx  ',
-    'MaternalLocator',
-    'MaternalPostFu',
-    'MaternalPostFuDx ',
-    'MaternalVisit',]
 
-    print '""" admin """'        
+    print '"""" admin """"'        
     for m in models:
         gen_admin( model=m, base='MyModelAdmin', )
         
         
-    print '""" forms """'        
+    print '"""" forms """"'        
     for m in models:
         gen_form( model=m, base='forms.ModelForm', )        
+"""
