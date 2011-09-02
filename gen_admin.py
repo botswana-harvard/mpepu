7b29c51f29dbe1b677ce69795cdfe9eda3d328e7
erik van widenfelt <ew2789@gmail.com>
2011-09-02 08:01:17 +0000

    git-svn-id: http://192.168.1.50/svn/bhp056_project@51 7d2a49fb-c946-49a1-8d64-222705d2c8ab

--- gen_admin.py
+++ gen_admin.py
@@ -22,6 +22,10 @@ def gen_admin( **kwargs):
     #foreign_key_field = kwargs.get("foreign_key", "maternal_arv_preg_history")
     #base_model_admin = kwargs.get("base", 'MyMaternalVisitModelAdmin')
 
+    app_label = kwargs.get("app_label", 'mpepu_lab')
+    foreign_key_field = kwargs.get("foreign_key", "id")
+    base_model_admin = kwargs.get("base", 'MyMaternalModelAdmin')
+
     #app_label = 'mpepu_maternal'
     #foreign_key_field = "registered_subject"
     #base_model_admin = 'MyRegisteredSubjectModelAdmin'
