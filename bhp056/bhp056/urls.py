2ae6bcea1a93ad7b891f2556bc909e3d19457643
Twice Tshwenyane <twicet@gmail.com>
2014-02-25 14:36:30 +0200

    work in progress on refs #370

--- bhp056/bhp056/urls.py
+++ bhp056/bhp056/urls.py
@@ -87,8 +87,14 @@ urlpatterns += patterns('',
     url(r'^{app_name}/$'.format(app_name=APP_NAME), RedirectView.as_view(url='/{app_name}/section/'.format(app_name=APP_NAME))),
     url(r'', RedirectView.as_view(url='/{app_name}/section/'.format(app_name=APP_NAME))),
     )
+from django.conf import settings
+from django.conf.urls import include, patterns, url
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
