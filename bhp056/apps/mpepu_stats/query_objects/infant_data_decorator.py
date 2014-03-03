049ed87ac217eecf331d57b75a11b70baa7a1e7e
Twice Tshwenyane <twicet@gmail.com>
2014-03-03 17:37:01 +0200

    fixed infant hospitalization links

--- bhp056/apps/mpepu_stats/query_objects/infant_data_decorator.py
+++ bhp056/apps/mpepu_stats/query_objects/infant_data_decorator.py
@@ -3,8 +3,9 @@ from apps.mpepu_maternal.models.maternal_consent import MaternalConsent
 
 class InfantDataDecorator(object):
 
-    def __init__(self, infant_data):
+    def __init__(self, infant_data, **kwargs):
         self.infant = infant_data
+        self.__data = kwargs
 
     def maternalconsent(self):
         return MaternalConsent.objects.filter(subject_identifier=self.relative_identifier())[0]
@@ -23,3 +24,7 @@ class InfantDataDecorator(object):
 
     def infant_birth_id(self):
         return self.registered_subject().infantbirth.pk
+
+    @property
+    def data(self):
+        return self.__data
