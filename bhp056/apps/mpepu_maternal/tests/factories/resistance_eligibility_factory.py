fabbc6a02e9ab21aae281596204fbe1ba7610f90
melissa kewagamang <mkewagamang@bhp.org.bw>
2014-03-24 12:33:09 +0200

    testing

--- bhp056/apps/mpepu_maternal/tests/factories/resistance_eligibility_factory.py
+++ bhp056/apps/mpepu_maternal/tests/factories/resistance_eligibility_factory.py
@@ -1,8 +1,11 @@
 import factory
 from apps.mpepu_maternal.models import ResistanceEligibility
+from .maternal_visit_factory import MaternalVisitFactory
 
 
 class ResistanceEligibilityFactory(factory.Factory):
     FACTORY_FOR = ResistanceEligibility
+
+    maternal_visit = factory.SubFactory(MaternalVisitFactory)
     stopped_arv = 'No'
     incarcerated = 'No'
--- bhp056/apps/mpepu_maternal/tests/resistance_eligibility_tests.py
+++ bhp056/apps/mpepu_maternal/tests/resistance_eligibility_tests.py
@@ -1,34 +1,33 @@
-from datetime import date
-from dateutil.relativedelta import relativedelta
+from django.contrib import admin
+from datetime import datetime
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from ..models import ResistanceEligibility
-# from .factories import ResistanceEligibilityFactory
+from .factories import ResistanceEligibilityFactory
+
+from edc.lab.lab_profile.classes import site_lab_profiles
+from edc.subject.visit_schedule.classes import site_visit_schedules
+from apps.mpepu.mpepu_app_configuration.classes import MpepuAppConfiguration
 
 
 class ResistanceEligibilityTests(TestCase):
 
     def test_maternal_eligibility(self):
+
+        admin.autodiscover()
+        site_lab_profiles.autodiscover()
+        MpepuAppConfiguration()
+        site_visit_schedules.autodiscover()
+        site_visit_schedules.build_all()
+
         """when mother is eligible"""
         self.assertEquals(0, ResistanceEligibility.objects.count(), "There should be no saved Resistance Eligibility objects")
-        ResistanceEligibility.objects.create(
+        ResistanceEligibilityFactory(
                                         co_enrolled='Yes',
                                         status_evidence='Yes',
                                         lates_cd4=250,
                                         who_illness='No'
                                         )
         self.assertEquals(1, ResistanceEligibility.objects.count(), "There should be one Eligibility saved.")
-
-
-#     def test_maternal_ineligibility(self):
-#         """when mother is ineligible"""
-#         self.assertEquals(0, ResistanceEligibility.objects.count(), "There should be no saved Resistance Eligibility objects")
-#         ResistanceEligibilityFactory.create(
-#                                         co_enrolled = 'Yes',
-#                                         status_evidence = 'Yes',
-#                                         lates_cd4 = 250,
-#                                         who_illness = 'No'
-#                                         )
-#         self.assertEquals(1, ResistanceEligibility.objects.count(), "There should be one Eligibility saved.")
