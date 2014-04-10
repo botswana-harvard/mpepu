fad91d68b6a1b59e98097e5315c674945dccb083
melissa kewagamang <mkewagamang@bhp.org.bw>
2014-04-10 17:37:39 +0200

    update requisitions forms and excluded un-used field from requisitions forms

--- bhp056/apps/mpepu_lab/admin/base_maternal_requisition_model_admin.py
+++ bhp056/apps/mpepu_lab/admin/base_maternal_requisition_model_admin.py
@@ -7,3 +7,9 @@ class BaseMaternalRequisitionModelAdmin (BaseRequisitionModelAdmin):
     visit_model = MaternalVisit
     visit_fieldname = 'maternal_visit'
     dashboard_type = 'maternal'
+
+    def __init__(self, *args, **kwargs):
+        super(BaseMaternalRequisitionModelAdmin, self).__init__(*args, **kwargs)
+        # remove these fields from admin fields list, default values should apply
+        for fld in ['test_code']:
+            self.fields.remove(fld)
