e12a6b860f0edfb599bdb7ff1e368d2674e46b33
Twice Tshwenyane <twicet@gmail.com>
2014-04-25 09:18:25 +0200

    updated eligible infants to use DOL

--- bhp056/apps/mpepu_stats/views/eligible_infants.py
+++ bhp056/apps/mpepu_stats/views/eligible_infants.py
@@ -6,29 +6,30 @@ from django.views.decorators.http import require_GET
 from ..query_objects.infant_randomization import NonRandomizedInfants
 
 
-DEFAULT_DATE_FORMAT = "%Y-%m-%d"
-
-
-def date_from_str(date_str, date_format=DEFAULT_DATE_FORMAT):
-    return datetime.strptime(date_str, date_format).date()
-
-
 @login_required
 @require_GET
 def eligible_infants(request, **kwargs):
     template = 'eligible_infants.html'
-    search_dt = request.GET.get('search_date')
-    context = {}
-    search_date = date.today()
-    if search_dt:
-        search_date = date_from_str(search_dt)
-    site = request.GET.get('site')
-    if site:
-        context['site'] = site
-    infants = NonRandomizedInfants(site=site, search_date=search_date)
-    eligibles = infants.eligibles
-    context['infants'] = eligibles
-    context['infants_count'] = infants.eligible_count
-    context['search_date'] = search_date
-    context['today'] = date.today().strftime('%Y-%m-%d')
+    site = _site(request)
+    search_date = _search_date(request)
+    infants = NonRandomizedInfants(site=site, search_date=search_date).query()
+    context = {'infants': infants,
+               'search_date': search_date,
+               'today': date.today().strftime('%Y-%m-%d'),
+               'site': site
+               }
     return render(request, template, context)
+
+
+def _site(request):
+    return request.GET.get('site')
+
+
+def _search_date(request):
+    search_date_param = request.GET.get('search_date')
+    search_date = _date_from_text(search_date_param) if search_date_param else date.today()
+    return search_date
+
+
+def _date_from_text(date_str, date_format="%Y-%m-%d"):
+    return datetime.strptime(date_str, date_format).date()
