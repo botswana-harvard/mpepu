7598d7fe980d2f07fb7dc523dcd03788240517b4
Florence Chilisa <fchilisa@bhp.org.bw>
2014-04-29 12:32:50 +0200

    metada status test

--- bhp056/apps/mpepu_maternal/tests/factories/maternal_post_fu_factory.py
+++ bhp056/apps/mpepu_maternal/tests/factories/maternal_post_fu_factory.py
@@ -1,12 +1,9 @@
 import factory
 
-from edc.subject.registration.tests.factories import BaseRegistrationFactory
 from edc.base.model.tests.factories import BaseUuidModelFactory
 
-from apps.mpepu_maternal.models import MaternalPostFu
+from apps.mpepu_maternal.models import MaternalPostFu, MaternalPostFuDx
 from apps.mpepu_maternal.tests.factories import MaternalVisitFactory
-
-from edc.subject.registration.tests.factories.registered_subject_factory import RegisteredSubjectFactory
 
 
 class MaternalPostFuFactory(BaseUuidModelFactory):
@@ -19,3 +16,13 @@ class MaternalPostFuFactory(BaseUuidModelFactory):
     breastfeeding = 'No'
     has_chronic_cond = 'No'
     started_ctx = 'No'
+
+
+class MaternalPostFuDxFactory(BaseUuidModelFactory):
+    FACTORY_FOR = MaternalPostFuDx
+
+    maternal_post_fu = factory.SubFactory(MaternalPostFuFactory)
+    maternal_visit = factory.SubFactory(MaternalVisitFactory)
+    mother_hospitalized = 'No'
+    new_diagnoses = 'No'
+    who_clinical_stage = 'No'
