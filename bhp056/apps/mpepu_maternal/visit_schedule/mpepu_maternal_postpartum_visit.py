6cf53db4833b58e02fc953e9727532a2bdbc7fa1
Florence Chilisa <fchilisa@bhp.org.bw>
2014-05-28 10:36:46 +0200

    visit schedules

--- bhp056/apps/mpepu_maternal/visit_schedule/mpepu_maternal_postpartum_visit.py
+++ bhp056/apps/mpepu_maternal/visit_schedule/mpepu_maternal_postpartum_visit.py
@@ -281,6 +281,6 @@ class MpepuMaternalPostPartumVisitSchedule(VisitScheduleConfiguration):
                 EntryTuple(50L, u'mpepu_maternal', u'postnatalinfantfeedingsurvey', REQUIRED, NOT_ADDITIONAL),
                 #following are additional forms
                 EntryTuple(200L, u'mpepu_maternal', u'maternaldeath', NOT_REQUIRED, ADDITIONAL),
-                EntryTuple(210L, u'mpepu_maternal', u'maternaloffstudy', NOT_REQUIRED, ADDITIONAL),
+                EntryTuple(210L, u'mpepu_maternal', u'maternaloffstudy', REQUIRED, NOT_ADDITIONAL),
             )}
 site_visit_schedules.register(MpepuMaternalPostPartumVisitSchedule)
