7b224183f5c123049fe8688e1ecf9fcb5a13dffd
Florence Chilisa <fchilisa@bhp.org.bw>
2014-07-11 11:59:33 +0200

    CSV export + pagination

--- bhp056/apps/mpepu_maternal/admin/registered_subject_model_admin.py
+++ bhp056/apps/mpepu_maternal/admin/registered_subject_model_admin.py
@@ -1,6 +1,20 @@
+from collections import OrderedDict
+
+from edc.export.actions import export_as_csv_action
 from edc.subject.registration.admin import BaseRegisteredSubjectModelAdmin
 
 
 class RegisteredSubjectModelAdmin (BaseRegisteredSubjectModelAdmin):
     """ModelAdmin subclass for models with a ForeignKey to 'registered_subject'"""
     dashboard_type = 'maternal'
+
+    actions = [export_as_csv_action(description="CSV Export of registered_subject",
+        fields=[],
+        delimiter=',',
+        exclude=['created', 'modified', 'user_created', 'user_modified', 'revision', 'id', 'hostname_created', 'hostname_modified' ],
+        extra_fields=OrderedDict(
+            {'subject_identifier': 'registered_subject__subject_identifier',
+             'gender': 'registered_subject__gender',
+             'dob': 'registered_subject__dob',
+             'registered': 'registered_subject__registration_datetime'}),
+        )]
